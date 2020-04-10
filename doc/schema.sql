a9d5653de9cea4c9666edd21603a3f575ee78009
cjbi <cjbi@outlook.com>
2020-04-10 14:21:03 +0800

    second commit

--- doc/schema.sql
+++ doc/schema.sql
@@ -1,65 +1,70 @@
-create table sys_log
-(
-    id          bigint auto_increment comment '主键'
-        primary key,
-    username    varchar(100)                       null comment '用户名',
-    ip          varchar(45)                        null comment '用户ip',
-    req_method  varchar(200)                       null comment '请求方法',
-    req_uri     text                               null comment '请求URL',
-    exec_method varchar(200)                       null comment '执行方法',
-    exec_time   bigint                             null comment '响应时间',
-    args        text                               null comment '参数',
-    return_val  text                               null comment '返回值',
-    exec_desc   varchar(200)                       null comment '描述',
-    status      varchar(45)                        null comment '状态',
-    create_time datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '创建时间'
-) comment '系统日志表';
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `username` varchar(100) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(100) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(100) DEFAULT NULL COMMENT '盐值',
+  `role_ids` varchar(100) DEFAULT NULL COMMENT '角色列表',
+  `locked` tinyint(1) DEFAULT '0' COMMENT '是否锁定',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_sys_user_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
-create table sys_permission
-(
-    id         bigint auto_increment comment '编号'
-        primary key,
-    name       varchar(100)         null comment '名称',
-    type       tinyint(1)           not null comment '资源类型(1.菜单 2.按钮或文本块)',
-    parent_id  bigint               null comment '父编号',
-    parent_ids varchar(100)         null comment '父编号列表',
-    permission varchar(100)         null comment '权限字符串',
-    icon       varchar(100)         null comment '图标',
-    sort       int        default 0 not null comment '排序',
-    status     tinyint(1) default 1 null comment '是否有效',
-    config     json                 null comment '权限配置'
-) comment '资源表';
+DROP TABLE IF EXISTS `t_user_detail`;
+CREATE TABLE `t_user_detail` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create index idx_sys_resource_parent_id
-    on sys_permission (parent_id);
+DROP TABLE IF EXISTS `t_user_tag`;
+CREATE TABLE `t_user_tag` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `column_4` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create index idx_sys_resource_parent_ids
-    on sys_permission (parent_ids);
-
-create table sys_role
-(
-    id             bigint auto_increment comment '编号'
-        primary key,
-    role           varchar(100)         not null comment '唯一标识',
-    name           varchar(100)         null comment '角色名称',
-    description    varchar(100)         null comment '描述',
-    status         tinyint(1) default 1 null comment '状态 1.正常 0.禁用',
-    permission_ids varchar(500)         null comment '资源编号列表'
-) comment '角色表';
-
-create index idx_sys_role_resource_ids
-    on sys_role (permission_ids);
-
-create table sys_user
-(
-    id       bigint auto_increment comment '编号'
-        primary key,
-    username varchar(100)         null comment '用户名',
-    password varchar(100)         null comment '密码',
-    salt     varchar(100)         null comment '盐值',
-    role_ids varchar(100)         null comment '角色列表',
-    locked   tinyint(1) default 0 null comment '是否锁定',
-    constraint idx_sys_user_username
-        unique (username)
-) comment '用户表';
-
+DROP TABLE IF EXISTS `t_goods`;
+CREATE TABLE `t_goods` (
+  `id` int(11) unsigned NOT NULL,
+  `category_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `goods_sn` varchar(60) NOT NULL DEFAULT '',
+  `name` varchar(120) NOT NULL DEFAULT '',
+  `brand_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `goods_number` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `keywords` varchar(255) NOT NULL DEFAULT '',
+  `goods_brief` varchar(255) NOT NULL DEFAULT '',
+  `goods_desc` text,
+  `is_on_sale` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `create_time` datetime DEFAULT NULL,
+  `sort_order` smallint(4) unsigned NOT NULL DEFAULT '100',
+  `is_delete` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `attribute_category` int(11) unsigned NOT NULL DEFAULT '0',
+  `counter_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '专柜价格',
+  `extra_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '附加价格',
+  `is_new` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `goods_unit` varchar(45) NOT NULL COMMENT '商品单位',
+  `primary_pic_url` varchar(255) NOT NULL COMMENT '商品主图',
+  `list_pic_url` varchar(255) NOT NULL COMMENT '商品列表图',
+  `retail_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '零售价格',
+  `sell_volume` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '销售量',
+  `primary_product_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '主sku　product_id',
+  `unit_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '单位价格，单价',
+  `promotion_desc` varchar(255) NOT NULL,
+  `promotion_tag` varchar(45) NOT NULL,
+  `app_exclusive_price` decimal(10,2) unsigned NOT NULL COMMENT 'APP专享价',
+  `is_app_exclusive` tinyint(1) unsigned NOT NULL COMMENT '是否是APP专属',
+  `is_limited` tinyint(1) unsigned NOT NULL,
+  `is_hot` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `goods_sn` (`goods_sn`),
+  KEY `cat_id` (`category_id`),
+  KEY `brand_id` (`brand_id`),
+  KEY `goods_number` (`goods_number`),
+  KEY `sort_order` (`sort_order`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
